25dcd848821e9840c12667449d7882cd5d048b81
Mo Afshar <moafshaar@gmail.com>
2020-03-09 17:50:22 +0000

    Week 1 - Valid Parantheses solution

---
+++ Mo/Week_1/20-Valid Parentheses.py
@@ -0,0 +1,40 @@
+"""
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1: 
+    Input: "()"
+    Output: true
+    
+Example 2: 
+    Input: "()[]{}"
+    Output: true   
+"""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if s == "": 
+            return True
+        
+        bracket_mapping = {')': '(', '}': '{', ']': '['}
+        stack = []
+        
+        for bracket in s:
+            if bracket not in bracket_mapping:
+                stack.append(bracket)
+            elif bracket_mapping[bracket] in stack: 
+                stack.pop()
+            else: 
+                return False
+        
+        # If the stack is empty we have closed all brackets correctly 
+        if not stack: 
+            return True 
+        # If the stack is not empty after finishing, the brackets did not 
+        # close in the correct order
+        return False
